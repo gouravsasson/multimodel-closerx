256d63fc9a9003000e5a5d59ac82123be8d3cb51
gourav sasson <sasson.gourav93@gmail.com>
2024-12-23 10:23:12 +0530

    import path in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,4 @@
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { defineConfig } from "vite";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, 'src/widget.tsx'),
-//       name: 'ReactWidget',
-//       fileName: 'react-widget',
-//       formats: ['iife'],
-//     },
-//     rollupOptions: {
-//       external: ['react', 'react-dom'],
-//       output: {
-//         globals: {
-//           react: 'React',
-//           'react-dom': 'ReactDOM',
-//         },
-//       },
-//     },
-//   },
-// });
-// import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react-swc";
 // import path from "path";
 
@@ -68,14 +39,10 @@
 //     ...commonConfig,
 //     build: {
 //       outDir: "dist/app",
-//     },
-//   };
-// });
-
-
-import path from "path";
+//     import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
